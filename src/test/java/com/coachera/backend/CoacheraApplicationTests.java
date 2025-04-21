9364fb7839c4a6a88e817af21da89ecf143bdae9
Mouaaz Sweid <mmsweid@gmail.com>
2025-04-21 23:34:52 +0300

    first commit

---
+++ src/test/java/com/coachera/backend/CoacheraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coachera.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoacheraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
