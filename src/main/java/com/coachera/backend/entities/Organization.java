fd5da909e98e918c48a4f1cda32581da81ce0d2f
Ken <ahmadsoud63@gmail.com>
2025-04-23 21:29:55 +0300

    add org entity

---
+++ src/main/java/com/coachera/backend/entities/Organization.java
@@ -0,0 +1,30 @@
+package com.coachera.backend.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "organizations")
+//@Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
+public class Organization extends Auditable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @OneToOne
+    @JoinColumn(name = "user_id", nullable = false, unique = true)
+    private User user;
+
+    @Column(nullable = false)
+    private String orgName;
+
+    @Column(nullable = false)
+    private String orgDescription;
+}
+
