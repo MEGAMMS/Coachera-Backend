e934ce0c04ae299dee3eadf1725d0bf174900891
Ken <ahmadsoud63@gmail.com>
2025-04-23 22:11:45 +0300

    add certificate entity

---
+++ src/main/java/com/coachera/backend/entities/Certificate.java
@@ -0,0 +1,36 @@
+package com.coachera.backend.entities;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "certificates")
+//@Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
+public class Certificate extends Auditable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "course_id", nullable = false)
+    private Course course;
+
+    @ManyToOne
+    @JoinColumn(name = "student_id", nullable = false)
+    private Student student;
+
+    @Column(nullable = false)
+    private LocalDate issuedAt;
+
+    @Column(nullable = false)
+    private String certificateUrl;
+}
+
