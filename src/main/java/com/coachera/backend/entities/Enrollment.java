16da4ac114a486d8f5e41468d40d958124fc0dfa
Ken <ahmadsoud63@gmail.com>
2025-04-23 22:09:44 +0300

    add Enrollment entity

---
+++ src/main/java/com/coachera/backend/entities/Enrollment.java
@@ -0,0 +1,31 @@
+package com.coachera.backend.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "enrollments")
+//@Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
+public class Enrollment extends Auditable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "course_id", nullable = false)
+    private Course course;
+
+    @ManyToOne
+    @JoinColumn(name = "student_id", nullable = false)
+    private Student student;
+
+    @Column(nullable = false)
+    private String progress;
+}
+
