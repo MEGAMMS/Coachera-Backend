f776ab31d434197838a02676ad27b6982750e9b3
Ken <ahmadsoud63@gmail.com>
2025-04-23 22:04:07 +0300

    add coursecategory entity

---
+++ src/main/java/com/coachera/backend/entities/CourseCategory.java
@@ -0,0 +1,27 @@
+package com.coachera.backend.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "categories_course")
+//@Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
+public class CourseCategory extends Auditable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "course_id", nullable = false)
+    private Course course;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id", nullable = false)
+    private Category category;
+}
+
