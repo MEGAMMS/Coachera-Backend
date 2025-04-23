c549f99c627f25a03babcdaf78a4b32c10a85334
Ken <ahmadsoud63@gmail.com>
2025-04-23 18:45:12 +0300

    add auditable (base entity) classs

--- src/main/java/com/coachera/backend/config/AuditingConfig.java
+++ src/main/java/com/coachera/backend/config/AuditingConfig.java
@@ -13,7 +13,7 @@ public class AuditingConfig {
 
     @Bean
     public AuditorAware<String> auditorProvider() {
-        
+        // for the created by and updated by columns if added
         return () -> Optional.of("system"); // TODO : when incorprating spring security edit this to match it
     }
 }
