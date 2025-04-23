ffda3093aefb0210cd8e1c4e4060d3267c379ce9
Ken <ahmadsoud63@gmail.com>
2025-04-23 21:10:55 +0300

    add student entity

---
+++ src/main/java/com/coachera/backend/entities/Student.java
@@ -0,0 +1,41 @@
+package com.coachera.backend.entities;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "students")
+//@Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
+public class Student extends Auditable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @OneToOne
+    @JoinColumn(name = "user_id", nullable = false, unique = true)
+    private User user;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false)
+    private LocalDate birthDate;
+
+    @Column(nullable = false)
+    private String gender;
+
+    @Column(nullable = false)
+    private String education;
+}
+
