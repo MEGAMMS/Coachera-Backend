97ff431de7c461b3f3e13d86bff581da9a2bde12
Ken <ahmadsoud63@gmail.com>
2025-04-23 21:17:13 +0300

    add admin entity

---
+++ src/main/java/com/coachera/backend/entities/Admin.java
@@ -0,0 +1,27 @@
+package com.coachera.backend.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "admins")
+//@Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
+public class Admin extends Auditable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @OneToOne
+    @JoinColumn(name = "user_id", nullable = false, unique = true)
+    private User user;
+
+    @Column(nullable = false)
+    private String adminName;
+}
+
