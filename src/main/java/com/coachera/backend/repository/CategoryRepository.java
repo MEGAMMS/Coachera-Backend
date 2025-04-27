3f70c80ba04723e72b04cd2f6309591a62dad12e
Mouaaz Sweid <mmsweid@gmail.com>
2025-04-27 20:38:41 +0300

    fix for category repo

--- src/main/java/com/coachera/backend/repository/CategoryRepository.java
+++ src/main/java/com/coachera/backend/repository/CategoryRepository.java
@@ -1,9 +1,10 @@
 package com.coachera.backend.repository;
 
-import java.util.Locale.Category;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.coachera.backend.entity.Category;
+
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
-    
+
 }
