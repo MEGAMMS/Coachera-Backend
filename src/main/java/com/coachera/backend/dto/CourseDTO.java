e38dc94a3cfc1cc2df65dab96582b190a9b3ba15
Mouaaz Sweid <mmsweid@gmail.com>
2025-05-04 03:31:55 +0300

    add docs

--- src/main/java/com/coachera/backend/dto/CourseDTO.java
+++ src/main/java/com/coachera/backend/dto/CourseDTO.java
@@ -1,11 +1,18 @@
 package com.coachera.backend.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
 public class CourseDTO {
+    @Schema(accessMode = Schema.AccessMode.READ_ONLY, example = "3")
     private Integer id;
+
+    @Schema(example = "Java totorial")
     private String title;
+
+    @Schema(example = "hello guys and welcome back.....")
     private String description;
+
     // private Integer orgId;
 }
