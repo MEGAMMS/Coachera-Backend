5ccfe690edc990b33379a2ce1f711493e4c850d9
Ken <ahmadsoud63@gmail.com>
2025-05-08 15:04:08 +0300

    add enrollment dto

---
+++ src/main/java/com/coachera/backend/dto/EnrollmentDTO.java
@@ -0,0 +1,38 @@
+package com.coachera.backend.dto;
+
+import com.coachera.backend.entity.Enrollment;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = false)
+@Schema(description = "Enrollment Data Transfer Object")
+public class EnrollmentDTO extends AuditableDTO {
+
+    @Schema(accessMode = Schema.AccessMode.READ_ONLY, example = "1", description = "Unique identifier of the enrollment")
+    private Integer id;
+
+    @Schema(required = true, description = "Associated course")
+    private CourseDTO course;
+
+    @Schema(required = true, description = "Associated student")
+    private StudentDTO student;
+
+    @Schema(required = true, example = "75%", description = "Student's course progress")
+    private String progress;
+
+    // Constructor from entity
+    public EnrollmentDTO(Enrollment enrollment) {
+        this.id = enrollment.getId();
+        this.course = new CourseDTO(enrollment.getCourse());
+        this.student = new StudentDTO(enrollment.getStudent());
+        this.progress = enrollment.getProgress();
+        this.setCreatedAt(enrollment.getCreatedAt());
+        this.setUpdatedAt(enrollment.getUpdatedAt());
+    }
+}
