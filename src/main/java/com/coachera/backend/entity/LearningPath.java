cfd5f6b915a4506ff27620d68e02f45c18470e17
tariq_SP <teto75928@gmail.com>
2025-05-14 22:44:59 +0300

    Create learning_path enity and Repo

---
+++ src/main/java/com/coachera/backend/entity/LearningPath.java
@@ -0,0 +1,27 @@
+package com.coachera.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "learning_paths")
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class LearningPath {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "org_id", nullable = false)
+    private Organization organization;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    private String image;
+}
