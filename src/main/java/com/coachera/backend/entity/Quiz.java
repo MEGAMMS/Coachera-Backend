bb5d4f97c08ced5a5cb33d40c16cbace552c1ad8
tariq_SP <teto75928@gmail.com>
2025-05-14 21:42:30 +0300

    Create quiz enity and Repo

---
+++ src/main/java/com/coachera/backend/entity/Quiz.java
@@ -0,0 +1,19 @@
+package com.coachera.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "quizzes")
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Quiz {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "material_id", nullable = false, unique = true)
+    private Material material;
+}
