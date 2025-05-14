098d62c795af5410bc51130fae8b8ec0404429c0
tariq_SP <teto75928@gmail.com>
2025-05-14 21:00:56 +0300

    Create instructer enity and Repo

---
+++ src/main/java/com/coachera/backend/entity/Instructor.java
@@ -0,0 +1,29 @@
+package com.coachera.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Entity
+@Table(name = "instructors")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Instructor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @OneToOne
+    @JoinColumn(name = "user_id", nullable = false ,unique = true)
+    private User user;
+
+    @Column(columnDefinition = "TEXT")
+    private String bio;
+
+}
