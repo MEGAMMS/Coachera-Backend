642783f03257508129d53422b3ff4b32982e87ce
Ken <ahmadsoud63@gmail.com>
2025-05-23 13:16:31 +0300

    add image generaor

---
+++ src/main/java/com/coachera/backend/generator/ImageGenerator.java
@@ -0,0 +1,25 @@
+package com.coachera.backend.generator;
+
+import java.util.UUID;
+import java.util.concurrent.ThreadLocalRandom;
+
+import org.instancio.Instancio;
+import org.instancio.Select;
+
+import com.coachera.backend.entity.Image;
+
+public class ImageGenerator {
+    public static Image createImage() {
+        return Instancio.of(Image.class)
+            .ignore(Select.field(Image::getId))
+            .supply(Select.field(Image::getUuidName), () -> generateImageUuid())
+            .create();
+    }
+
+    private static String generateImageUuid() {
+        // Format: "img_" + UUID + "_" + timestamp
+        return String.format("img_%s_%d",
+                UUID.randomUUID().toString().substring(0, 8),
+                ThreadLocalRandom.current().nextInt(1000, 9999));
+    }
+}
