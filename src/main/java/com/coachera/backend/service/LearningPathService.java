0568500a3b22d3a87f9794a2e669699d443a5ad6
Ken <ahmadsoud63@gmail.com>
2025-06-01 10:26:38 +0300

    add pagination to learning path

--- src/main/java/com/coachera/backend/service/LearningPathService.java
+++ src/main/java/com/coachera/backend/service/LearningPathService.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -69,6 +71,11 @@ public class LearningPathService {
         return learningPathRepository.findAll().stream()
                 .map(learningPath -> new LearningPathDTO(learningPath))
                 .collect(Collectors.toList());
+    }
+
+    public Page<LearningPathDTO> getLearningPaths(Pageable pageable) {
+        return learningPathRepository.findAll(pageable)
+                .map(LearningPathDTO::new);
     }
 
     public List<LearningPathDTO> getLearningPathsByOrganization(Integer organizationId) {
