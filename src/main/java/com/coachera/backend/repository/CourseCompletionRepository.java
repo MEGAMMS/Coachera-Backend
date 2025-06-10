6300381c698e87ac2a67f440df5a35c750b1a7dd
Ken <ahmadsoud63@gmail.com>
2025-06-10 10:44:19 +0300

    add repos

---
+++ src/main/java/com/coachera/backend/repository/CourseCompletionRepository.java
@@ -0,0 +1,8 @@
+package com.coachera.backend.repository;
+
+import com.coachera.backend.entity.CourseCompletion;
+import com.coachera.backend.entity.Enrollment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CourseCompletionRepository extends JpaRepository<CourseCompletion, Enrollment> {
+}
