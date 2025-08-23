6c2e01b3a9dc3d62f6c201512afe9a536ca279f7
tariq_SP <teto75928@gmail.com>
2025-08-23 15:57:48 +0300

    add admin Folders for dashboard endpoint

--- src/main/java/com/coachera/backend/controller/admin/AdminController.java
+++ src/main/java/com/coachera/backend/controller/admin/AdminController.java
@@ -1,4 +1,4 @@
-package com.coachera.backend.controller;
+package com.coachera.backend.controller.admin;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
---
+++ src/main/java/com/coachera/backend/controller/admin/OrganizationAdminController.java
@@ -0,0 +1,5 @@
+package com.coachera.backend.controller.admin;
+
+public class OrganizationAdminController {
+
+}
